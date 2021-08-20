787b3d77c04dd14bbb5f7a7fa692d810fcaa3689
Himanshu <hd156681@gmail.com>
2021-08-20 16:28:14 +0530

    thirs commit

--- src/main/java/cicdlab2/cicdlab2.java
+++ src/main/java/cicdlab2/cicdlab2.java
@@ -6,6 +6,7 @@ public class cicdlab2 {
 		// TODO Auto-generated method stub
 System.out.println("Hello , jenkins");
 System.out.println("Hello, ops");
+System.out.println(("This is the thid push for the third build"));
 	}
 
 }
